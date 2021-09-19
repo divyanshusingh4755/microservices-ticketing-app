8df773f450c6a0765628762c3582594fb62a7b1d
divyanshusingh4755 <divyanshusingh4755@gmail.com>
2021-09-20 05:04:38 +0530

    Compelted Login and Sign Up from frontend to backend

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,18 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-const AppComponent = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
     return (
-        <div>
+        <div className="container-fluid">
+            <Header currentUser={currentUser} />
             <Component {...pageProps} />
         </div>
     )
 };
 
 AppComponent.getInitialProps = async (context) => {
-    const client = buildClient(context);
+    const client = buildClient(context.ctx);
     const { data } = await client.get("/api/users/currentuser");
-    return data;
+
+    let pageProps = {};
+
+    if (context.Component.getInitialProps) {
+        pageProps = await context.Component.getInitialProps(context.ctx);
+    }
+    return {
+        pageProps,
+        ...data
+    }
 }
 
 export default AppComponent;
