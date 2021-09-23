fdc48ce627606916c6d99537589cdd45a9127569
divyanshusingh4755 <divyanshusingh4755@gmail.com>
2021-09-23 20:02:10 +0530

    Added test cases for ticketing service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import 'express-async-errors';
 import cookieSession from "cookie-session";
+import { createTicketRouter } from "./routes/new";
 
-import { errorHandler, NotFoundError } from "@wikietickets/common";
+import { errorHandler, NotFoundError, currentUser } from "@wikietickets/common";
 
 const app = express();
 app.set("trust proxy", true);
@@ -10,9 +11,11 @@ app.use(express.json());
 app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== "test"
-}))
+}));
+app.use(currentUser);
 
 // importing routes
+app.use(createTicketRouter);
 
 app.all("*", async (req, res) => {
     throw new NotFoundError()
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,20 +1,89 @@
 import request from "supertest";
 import { app } from "../../app";
+import jwt from "jsonwebtoken";
+
+function signIn() {
+    // Build a JWT payload. {id, email}
+    const payload = {
+        id: "123456",
+        email: "test@test.com"
+    };
+
+    // Create the JWT
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+    // Build session Object, {jwt:MY_JWT}
+    const session = { jwt: token };
+
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session)
+
+    // Take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString("base64");
+
+    return [`express:sess=${base64}`];
+}
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
+    const response = await request(app)
+        .post("/api/tickets")
+        .send({});
 
+    expect(response.status).not.toEqual(404);
 });
 
 it("can only be accessed if the user is signed in", async () => {
-
+    const response = await request(app)
+        .post("/api/tickets")
+        .send({})
+        .expect(401);
 });
+
+it("returns a status other than 401 if user is signed in", async () => {
+    const response = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", signIn())
+        .send({})
+
+    expect(response.status).not.toEqual(401);
+})
 
 it("returns an error if an invalid title is provided", async () => {
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", signIn())
+        .send({
+            title: "",
+            price: 10
+        })
+        .expect(400);
 
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", signIn())
+        .send({
+            price: 10
+        })
+        .expect(400);
 });
 
-it("returns an error if an invlaid price is provided", async () => {
+it("returns an error if an invalid price is provided", async () => {
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", signIn())
+        .send({
+            title: "test",
+            price: ''
+        })
+        .expect(400);
 
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", signIn())
+        .send({
+            title: "test"
+        })
+        .expect(400);
 });
 
 it("created a ticket with valid parameters", async () => {
